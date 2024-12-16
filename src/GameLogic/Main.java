dc3f8d4f28d6d09bc6d2b2654bc1dc35aa99ffed
MasonSimpson <masonrsimpson@gmail.com>
2024-12-16 16:08:29 -0600

    Started work on main menu. Added music that will be used as the main menu music.

--- src/GameLogic/Main.java
+++ src/GameLogic/Main.java
@@ -6,6 +6,15 @@ import javax.swing.*;
 public class Main {
 
     public static void main (String[]args) throws InterruptedException {
+        MainMenuPanel menuPanel = new MainMenuPanel();
+
+        JFrame frame = new JFrame("Realms");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1600, 1000);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.add(menuPanel);
+        frame.setVisible(true);
 
 
     }
--- src/GameLogic/MainMenuPanel.java
+++ src/GameLogic/MainMenuPanel.java
@@ -4,11 +4,38 @@ import javax.swing.*;
 import java.awt.*;
 import PlayerClasses.*;
 
-public class MainMenuPanel {
+public class MainMenuPanel extends JPanel {
     // Declaring Swing objects being used on the MainMenuPanel
     private JLabel titleLabel;
-    private Image gameLogo;
+    private Icon gameLogo;
     private JButton playButton;
+    private JButton rulesButton;
     private JButton optionsButton;
     private JButton exitButton;
+
+    public MainMenuPanel() {
+        super();
+        this.setLayout(null);
+        this.setBackground(Color.BLACK);
+
+        showMenu();
+    }
+
+    private void showMenu() {
+        // Game logo
+        gameLogo = new ImageIcon("src/Images/realms logo.png");
+        titleLabel = new JLabel(gameLogo);
+        titleLabel.setBounds(500, 20, 600, 300);
+        this.add(titleLabel);
+
+        // Play button
+        playButton = new JButton("Play Game");
+        playButton.setBounds(730, 400,150, 50);
+        playButton.setFont(new Font("Arial", Font.BOLD, 20));
+        playButton.setBackground(Color.BLACK);
+        playButton.setForeground(Color.white);
+        //playButton.addActionListener(e -> )
+        this.add(playButton);
+
+    }
 }
