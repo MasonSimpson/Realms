ac15211cf30a786980b626bea7b1793f203246c3
MasonSimpson <masonrsimpson@gmail.com>
2024-12-16 17:44:00 -0600

    Deleted rules.java as it won't be needed. Added images to make buttons look better than just plain arial font. Music fully works and has a volume slider in options menu

--- src/GameLogic/MainMenuPanel.java
+++ src/GameLogic/MainMenuPanel.java
@@ -10,10 +10,14 @@ public class MainMenuPanel extends JPanel {
     private JLabel titleLabel;
     private Icon gameLogo;
     private JButton playButton;
-    private JButton rulesButton;
+    private JButton newGameButton;
+    private JButton loadGameButton;
     private JButton optionsButton;
+    private JButton backToMenuButton;
     private JButton exitButton;
-    private String menuMusicPath = "src/Music/menu music.mp3";
+    private JSlider musicVolumeSlider;
+    private String menuMusicPath = "src/Music/menu music.wav";
+    private boolean menuMusicPlaying = false;
 
     public MainMenuPanel() {
         super();
@@ -24,8 +28,14 @@ public class MainMenuPanel extends JPanel {
     }
 
     private void showMenu() {
-        // Start music
-        MusicPlayer.play(menuMusicPath);
+        this.removeAll();
+
+        // Stops playing music if music that isn't menu music is currently playing and then starts menu music
+        if (!menuMusicPlaying) {
+            MusicPlayer.play(menuMusicPath);
+            MusicPlayer.loop();
+            menuMusicPlaying = true;
+        }
 
         // Game logo
         gameLogo = new ImageIcon("src/Images/realms logo.png");
@@ -35,12 +45,93 @@ public class MainMenuPanel extends JPanel {
 
         // Play button
         playButton = new JButton("Play Game");
+        playButton.setHorizontalAlignment(SwingConstants.CENTER);
+        playButton.setVerticalAlignment(SwingConstants.CENTER);
+        playButton.setIcon(new ImageIcon("src/Images/play game button.png"));
         playButton.setBounds(730, 400,150, 50);
-        playButton.setFont(new Font("Arial", Font.BOLD, 20));
         playButton.setBackground(Color.BLACK);
-        playButton.setForeground(Color.white);
-        //playButton.addActionListener(e -> )
+        playButton.addActionListener(e -> showGameMenu());
         this.add(playButton);
 
+        // Options button
+        optionsButton = new JButton("Options");
+        optionsButton.setHorizontalAlignment(SwingConstants.CENTER);
+        optionsButton.setVerticalAlignment(SwingConstants.CENTER);
+        optionsButton.setIcon(new ImageIcon("src/Images/options button.png"));
+        optionsButton.setBounds(730, 500,150, 50);
+        optionsButton.setBackground(Color.BLACK);
+        optionsButton.addActionListener(e -> showOptionsMenu());
+        this.add(optionsButton);
+
+        // Exit game button
+        exitButton = new JButton("Exit Game");
+        exitButton.setHorizontalAlignment(SwingConstants.CENTER);
+        exitButton.setVerticalAlignment(SwingConstants.CENTER);
+        exitButton.setIcon(new ImageIcon("src/Images/exit game button.png"));
+        exitButton.setBounds(730, 600,150, 50);
+        exitButton.setBackground(Color.BLACK);
+        exitButton.addActionListener(e -> System.exit(0));
+        this.add(exitButton);
+
+        this.revalidate();
+        this.repaint();
+
+    }
+    private void showGameMenu() {
+        this.removeAll();
+        this.add(titleLabel);
+        newGameButton = new JButton("New Game");
+        newGameButton.setHorizontalAlignment(SwingConstants.CENTER);
+        newGameButton.setIcon(new ImageIcon("src/Images/new game button.png"));
+        newGameButton.setBounds(730, 400,150, 50);
+        newGameButton.setBackground(Color.black);
+        this.add(newGameButton);
+        loadGameButton = new JButton("Load Game");
+        loadGameButton.setHorizontalAlignment(SwingConstants.CENTER);
+        loadGameButton.setIcon(new ImageIcon("src/Images/load game button.png"));
+        loadGameButton.setBounds(730, 500,150, 50);
+        loadGameButton.setFont(new Font("Arial", Font.BOLD, 20));
+        loadGameButton.setBackground(Color.black);
+        loadGameButton.setForeground(Color.white);
+        this.add(loadGameButton);
+        createBackToMenuButton();
+        this.revalidate();
+        this.repaint();
+    }
+    private void showOptionsMenu() {
+        this.removeAll();
+        this.add(titleLabel);
+        JLabel musicVolumeLabel = new JLabel("Music Volume");
+        musicVolumeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        musicVolumeLabel.setIcon(new ImageIcon("src/Images/music volume label.png"));
+        musicVolumeLabel.setBounds(730, 400,150, 50);
+        this.add(musicVolumeLabel);
+        musicVolumeSlider = new JSlider(0, 100, (int) MusicPlayer.getCurrentVolume());
+        musicVolumeSlider.setBounds(670, 450,250, 50);
+        musicVolumeSlider.setMajorTickSpacing(1);
+        musicVolumeSlider.setMinorTickSpacing(1);
+        musicVolumeSlider.setPaintTicks(false);
+        musicVolumeSlider.setPaintLabels(false);
+        musicVolumeSlider.setBackground(Color.black);
+        musicVolumeSlider.setForeground(Color.white);
+        // Add a ChangeListener to the slider to update the volume
+        musicVolumeSlider.addChangeListener(e -> {
+            int volumeValue = musicVolumeSlider.getValue();
+            MusicPlayer.setVolume(volumeValue);
+        });
+        this.add(musicVolumeSlider);
+        createBackToMenuButton();
+        this.revalidate();
+        this.repaint();
+
+    }
+    private void createBackToMenuButton() {
+        backToMenuButton = new JButton("Back to Menu");
+        backToMenuButton.setHorizontalAlignment(SwingConstants.CENTER);
+        backToMenuButton.setIcon(new ImageIcon("src/Images/back to menu button.png"));
+        backToMenuButton.setBounds(705, 600,200, 50);
+        backToMenuButton.setBackground(Color.black);
+        backToMenuButton.addActionListener(e -> showMenu());
+        this.add(backToMenuButton);
     }
 }
--- src/Music/MusicPlayer.java
+++ src/Music/MusicPlayer.java
@@ -6,22 +6,49 @@ import java.io.IOException;
 
 public class MusicPlayer {
     private static Clip clip;
+    private static FloatControl volumeControl;
+    private static float currentVolume = 60;
 
     public static void play(String path) {
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(path));
             clip = AudioSystem.getClip();
             clip.open(audioInputStream);
+            volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            setVolume(currentVolume);
             clip.start();
         }
         catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
     }
-    public void stop() {
+
+    public static void setVolume(float volume) {
+        currentVolume = volume;
+        if (volumeControl != null)
+        {
+            // Convert volume percentage (0-100) to the scale the volume control expects
+            float min = volumeControl.getMinimum();
+            float max = volumeControl.getMaximum();
+            float range = max - min;
+            float gain = min + (range * (volume / 100f));
+            volumeControl.setValue(gain);
+        }
+    }
+
+    public static void stop() {
         if (clip != null && clip.isRunning())
         {
             clip.stop();
         }
+    }
+    public static void loop() {
+        if (clip != null && clip.isRunning())
+        {
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
+    }
+    public static float getCurrentVolume() {
+        return currentVolume;
     }
 }
